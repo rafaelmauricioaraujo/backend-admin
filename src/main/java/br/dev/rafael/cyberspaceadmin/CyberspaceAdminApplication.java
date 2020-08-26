78fedba80a92ab57d374c948538afe4b81f25072
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-08-25 22:39:39 -0300

    configuring spring boot admin

---
+++ src/main/java/br/dev/rafael/cyberspaceadmin/CyberspaceAdminApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rafael.cyberspaceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberspaceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberspaceAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rafael/cyberspaceadmin/CyberspaceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rafael.cyberspaceadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberspaceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
